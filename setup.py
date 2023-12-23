f1e6d6d6ddce0e4d4361b5ccffcf1519bd8d2c0f
abhiram1809 <sharmaabhiram1809@gmail.com>
2023-12-23 11:46:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
 from setuptools import setup, find_packages
 
-with open('README.md', 'r', encoding='utf-8') as file:
+with open("README.md", "r", encoding="utf-8") as file:
     long_description = file.read()
 
 setup(
-    name='Quant_AgentTools',
-    version='0.2.8',
-    description='Agentic Workflow with Quantized LLMs',
+    name="Quant_AgentTools",
+    version="0.2.9",
+    description="Agentic Workflow with Quantized LLMs",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=[
-        'pydantic',
-        'GPT4All'
-    ],
+    install_requires=["pydantic", "GPT4All"],
 )
