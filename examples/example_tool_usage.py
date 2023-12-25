63ececbb2dd533a80424ca3bcb9be0646e751f48
abhiram1809 <sharmaabhiram1809@gmail.com>
2023-12-25 15:46:42 +0530

    Update example_tool_usage.py

--- examples/example_tool_usage.py
+++ examples/example_tool_usage.py
@@ -1,13 +1,13 @@
 from Quant_AgentTools.agent_tools import AgentTools
 
-agent = AgentTools(model_name="mistral-7b-instruct-v0.1.Q4_0.gguf", threads=3)
+agent = AgentTools(model_name="mistral-7b-openorca.Q4_0.gguf", threads=4)
 
 
 def add(a, b):
     return a + b
 
 
-def multiply(a, b):
+def mul(a, b):
     return a * b
 
 
@@ -15,7 +15,7 @@ def div(a, b):
     return a / b
 
 
-agent.add_tool("mul", multiply, "Multiplies two numbers", "mul(a, b)")
-agent.add_tool("div", div, "Divides two numbers", "div(a, b)")
+agent.add_tool("multiply", mul, "Multiplies two numbers", "mul(a, b)")
+agent.add_tool("division", div, "Divides two numbers", "div(a, b)")
 
 print(agent.chat("What is 7200 divided by 36, then times 5?"))
